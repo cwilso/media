d8c5d78cd5e49a48adcb08c11e296cc02dc6e566
Chris Wilson <cwilso@gmail.com>
2014-03-07 15:15:20 -0800

    First round draft outline

--- slide_config.js
+++ slide_config.js
@@ -1,14 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Media APIs for the multi-platform Web',
-//    subtitle: 'Chrome Developer Summit',
-    eventTitle: '',
+    title: 'Media APIs for the Multi-platform Web',
+//    subtitle: 'Fluent 2014',
+    eventTitle: 'Fluent 2014',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    analytics: 'UA-44413410-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+  //  analytics: 'UA-44413410-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
     favIcon: 'images/google_developers_logo_tiny.png',
     // fonts: [
     //   'Open Sans:regular,semibold,italic,italicsemibold',
@@ -19,18 +19,13 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [
-    // {
-    //   name: 'Jan Linden',
-    //   company: 'Senior Product Manager, Google',
-    //   gplus: 'https://plus.google.com/101097675148849633109/posts'
-    // },
     {
-    name: 'Sam Dutton',
+    name: 'Chris Wilson',
     company: 'Developer Advocate, Google Chrome',
-    gplus: 'http://plus.samdutton.com',
-    twitter: '@sw12',
-    www: 'http://www.samdutton.com',
-    github: 'http://github.com/samdutton'
+    gplus: 'https://plus.google.com/+ChrisWilson/',
+    twitter: '@cwilso',
+    www: 'http://www.cwilso.com',
+    github: 'http://github.com/cwilso'
   }
   ]
 };
